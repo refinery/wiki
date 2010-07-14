16235f713408fa9d9a68032d788b8ad9b97f8ed7
stevenheidel <steven@livingskyweb.ca>
2010-07-14 10:59:31 -0600

    Changed news (from which this was copied) to wiki

--- generators/refinerycms-wiki/refinerycms_wiki_generator.rb
+++ generators/refinerycms-wiki/refinerycms_wiki_generator.rb
@@ -1,26 +1,28 @@
-class NewsGenerator < Rails::Generator::NamedBase
+class WikiGenerator < Rails::Generator::NamedBase
 
   def initialize(*runtime_args)
     # set first argument to the table's name so that the user doesn't have to pass it in.
-    runtime_args[0] = ["news_items"]
+    runtime_args[0] = ["wiki_pages"]
     super(*runtime_args)
   end
 
   def banner
-    "Usage: script/generate news"
+    "Usage: script/generate wiki"
   end
 
   def manifest
     record do |m|
       m.migration_template 'migration.rb', 'db/migrate',
-        :migration_file_name => "create_structure_for_news",
+        :migration_file_name => "create_structure_for_wiki",
         :assigns => {
-          :migration_name => "CreateStructureForNews",
-          :table_name => "news_items",
+          :migration_name => "CreateStructureForWiki",
+          :table_name => "wiki_pages",
           :attributes => [
             Rails::Generator::GeneratedAttribute.new("title", "string"),
             Rails::Generator::GeneratedAttribute.new("body", "text"),
-            Rails::Generator::GeneratedAttribute.new("publish_date", "datetime")
+            Rails::Generator::GeneratedAttribute.new("integer", "category_id"),
+            Rails::Generator::GeneratedAttribute.new("integer", "version"),
+            Rails::Generator::GeneratedAttribute.new("integer", "position")
           ]
         }
     end
